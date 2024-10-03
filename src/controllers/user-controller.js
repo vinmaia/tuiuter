fd3c2dea0a6d18d3ec8ce219625b490b74cfc152
Maia <vincecake008@gmail.com>
2024-10-03 10:29:28 -0300

    feat: finished

--- src/controllers/user-controller.js
+++ src/controllers/user-controller.js
@@ -6,7 +6,34 @@ export const login = async (req, res) => {
     const { email, password } = req.body;
 
     const user = await User.findOne({
-      email, 
+      email,
+    });
+    if (user && (await user.isValidPassword(password))) {
+      const token = generateToken(user);
+
+      res.json({
+        token,
+      });
+    } else {
+      res.sendStatus(404);
+    }
+  } catch (error) {
+    res.status(500).send(error.message);
+  }
+};
+
+export const signup = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    const user = await User.create({
+      user,
+      password,
+    });
+
+    const token = generateToken(user);
+
+    res.status(201).json({
+      token,
     });
   } catch (error) {
     res.status(500).send(error.message);
